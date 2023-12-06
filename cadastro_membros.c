5e8c0f8e622e53561243bf6a674f1315c5d8aa11
eduardanogueira19 <eduardanogueira812@gmail.com>
2023-12-06 16:46:44 -0300

    Update cadastro_membros.c

--- cadastro_membros.c
+++ cadastro_membros.c
@@ -1,39 +1,54 @@
 //programa para cadastrar membros da igreja
 #include<stdio.h>
 #include<stdlib.h>
-#include<locale.h>
 #include<string.h>
 
 //dados da pessoa
-/*struct cadastro{
+struct cadastro{
 	char nome[100];
 	char cpf[15];
-	int nascimento;
-	int congregacao;
-	char nomeCongregacao[50];
-}*/
+	char nascimento[20];
+};
 
-
+struct cadastro pessoa[99];
+qnt = 0;
 //função para o cadastro dessa pessoa
-//void cadastro;
+void cadastro()
+{
+	printf("Insira seu nome: \n");
+	fflush(stdin);
+	gets(pessoa[qnt].nome);
+	printf("Insira o CPF: \n");
+	fflush(stdin);
+	gets(pessoa[qnt].cpf);
+	printf("Insira a data de nascimento: \n");
+	fflush(stdin);
+	gets(pessoa[qnt].nascimento);
+	qnt++;
+	system("cls");
+	
+}
 //função para listar todos os membros
-//void listarMembros(){
-//}
-
-//int lerOpcao(){
-//	int op;
-//	mostrarMenuOpcao(); 
-//	scanf("%d", &op);
-//	return op;
-//}
+void listarMembros(){
+	int i;
+	
+	for(i=0; i<qnt; i++){
+		printf("Nome: %s \n", pessoa[i].nome);
+		printf("CPF: %s \n", pessoa[i].cpf);
+		printf("Nascimento: %s \n", pessoa[i].nascimento);
+		printf("======================================\n");
+	}
+	
+}
 
 
 
 int main(){
+
 	
-	setlocale(LC_ALL, "");
 	//variáveis
 	int op;
+
 	//menu principal
 	do{
 		printf("\t\t _________________________\n");
@@ -43,5 +58,19 @@ int main(){
 		printf("\t\t|3 - Sair                 |\n");
 		printf("\t\t|_________________________|\n");
 		scanf("%d", &op);
+		
+		switch(op){
+			case 1: 
+				cadastro();
+				system("cls");
+				break;
+			case 2:
+				listarMembros();
+				system("cls");
+				break;
+			default: 
+				printf("Opcap invalida");
+				break;
+		}
 	}while(op != 3);
 }
